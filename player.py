569f0c9bf868d173389ec67f91707707223d9240
Pooh <pooh@poohprod.ru>
2022-06-12 23:00:56 +0300

    last updates, to russian command and kill command

--- player.py
+++ player.py
@@ -9,21 +9,30 @@ import discord
 from youtube_dl import YoutubeDL
 from asyncio import sleep
 
-YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'False'}
-FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',  'options': '-vn'}
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'False'}
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+
+@bot.event
 async def on_ready():
-    sys.stdout.write('Бот запущен.\Команда: $play <link>')
+    print('Бот запущен и ожидает команду.\nКоманды:\n!play <ссылка> - воспроизводит плейлист\n!kill - убивает бота')
+
+
+# 
+# ЭТА КОМАНДА ВЫЗЫВАЕТ ПОМОЩЬ ПО БОТУ
+# 
+@bot.command(name='помоги')
+async def helpme(ctx):
+    await ctx.send(f'Команды:\n!play <ссылка> - воспроизводит плейлист\n!kill - убивает бота\n\nМожно и по русски:\n!включи, !помоги, !умри')
 
 
 # 
 # ЭТА КОМАНДА ЗАСТАВЛЯЕТ БОТА ПОДКЛЮЧИТЬСЯ К ГОЛОСОВОМУ КАНАЛУ И ВОСПРОИЗВЕСТИ ЗВУК ИЗ ССЫЛКИ
 # 
-@bot.command()
+@bot.command(name='включи')
 async def play(ctx, arg):
     global vc
 
@@ -35,7 +44,7 @@ async def play(ctx, arg):
 
     if vc.is_playing():
         # ЭТОТ КУСОК КОДА СТОПИТ ТЕКУЩУЮ МУЗЫКУ, ЕСЛИ ВОСПРОИЗВОДИТСЯ, И ВКЛЮЧАЕТ НОВУЮ
-        await ctx.send(f'{ctx.message.author.mention}, музыка уже проигрывается. Останавливаю, и включаю новую')
+        await ctx.send(f'{ctx.message.author.mention}, музыка уже проигрывается. Останавливаю, и включаю новую.\nДля воспроизведения другого плейлиста, отправь команду !play <ссылка>')
 
         vc.stop()
         with YoutubeDL(YDL_OPTIONS) as ydl:
@@ -52,7 +61,7 @@ async def play(ctx, arg):
         
     else:
         # ЭТОТ КУСОК КОДА ВОСПРОИЗВОДИТ МУЗЫКУ ПО ССЫЛКЕ
-        await ctx.send(f'{ctx.message.author.mention}, включаю музыку')
+        await ctx.send(f'{ctx.message.author.mention}, включаю музыку.\nДля воспроизведения другого плейлиста, отправь команду !play <ссылка>')
 
         with YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(arg, download=False)
@@ -65,6 +74,16 @@ async def play(ctx, arg):
             await sleep(1)
         if not vc.is_paused():
             await vc.disconnect()
+
+
+# 
+# ЭТА КОМАНДА ЗАСТАВЛЯЕТ БОТА СЪЕБАТЬСЯ
+# 
+@bot.command(name='умри')
+async def kill(ctx):
+    await ctx.send(f'Умер.')
+    print('Умер.')
+    await ctx.bot.close()
 
 
 dotenv_path = join(dirname(__file__), '.env')
