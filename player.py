54f822f6ac54500509fdeda074494cdecba4b003
NorthernBlow <rheinhart@yandex.ru>
2022-10-14 02:41:52 +0300

    обнова под модные обновления апишки

--- player.py
+++ player.py
@@ -11,9 +11,9 @@ from asyncio import sleep
 
 YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'False'}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+intents = discord.Intents(messages=True, guilds=True)
 
-
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(intents=intents, command_prefix='!')
 
 
 @bot.event
